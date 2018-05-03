e5d25cfdd21e2efd105dee2a014a5019de68f369
Glenn Condron <glennc@microsoft.com>
2018-05-02 18:53:48 -0700

    Comment queue creation.

--- src/WebUI/Startup.cs
+++ src/WebUI/Startup.cs
@@ -50,7 +50,7 @@ namespace WebUI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            CreateQueueIfNotExists();
+            //CreateQueueIfNotExists();
 
             if (env.IsDevelopment())
             {
