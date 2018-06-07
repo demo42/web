b18b785e47b7656ea69b3c060bfa0154b00ca432
Steve Lasker <StevenLasker@Hotmail.com>
2018-06-07 10:43:09 -0700

    reset tests

--- test/demo42tests/baseImageTests.cs
+++ test/demo42tests/baseImageTests.cs
@@ -20,7 +20,7 @@ namespace demo42tests{
 
             Assert.True(string.IsNullOrEmpty(valueTested) ||
                         valueTested == "Good" ||
-                        valueTested == "Amazing" ||
+                        //valueTested == "Amazing" ||
                         valueTested == "Great");
         }
     }
