6264d979f70d6c46224089b09e0b86c1efe20c7a
Steve Lasker <StevenLasker@Hotmail.com>
2018-06-07 10:42:12 -0700

    update tests

--- test/demo42tests/IndexTests.cs
+++ test/demo42tests/IndexTests.cs
@@ -7,13 +7,10 @@ using WebUI;
 using WebUI.Pages;
 using Xunit;
 
-namespace demo42tests
-{
-    public class IndexTests
-    {
+namespace demo42tests{
+    public class IndexTests    {
         [Fact]
-        public async Task Index_Post_Validates_Words()
-        {
+        public async Task Index_Post_Validates_Words(){
             var pageUnderTest = new IndexModel(new MockQueueClient(), new MockConfig());
 
             pageUnderTest.Data = "InvalidWordData";
@@ -23,41 +20,29 @@ namespace demo42tests
             Assert.Equal("Not saved. Nice try.", pageUnderTest.Message);
             Assert.IsAssignableFrom<Microsoft.AspNetCore.Mvc.RedirectToPageResult>(result);
         }
-
-        [Fact]
-        public void TestBackgroundColor()
-        {
-            var colorUnderTest = Environment.GetEnvironmentVariable("BACKGROUND_COLOR");
-            Assert.True(string.IsNullOrEmpty(colorUnderTest) ||
-                        colorUnderTest == "White" ||
-                        colorUnderTest == "Azure" );
-        }
     }
 
-    public class MockQueueClient : IQuoteClient
-    {
-        public Task<Quote> GetRandomQuote()
-        {
+    public class MockQueueClient : IQuoteClient{
+        public Task<Quote> GetRandomQuote(){
             throw new Exception();
         }
     }
 
-    public class MockConfig : IConfiguration
-    {
-        public string this[string key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public class MockConfig : IConfiguration{
+        public string this[string key] { 
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException(); 
+        }
 
-        public IEnumerable<IConfigurationSection> GetChildren()
-        {
+        public IEnumerable<IConfigurationSection> GetChildren(){
             throw new NotImplementedException();
         }
 
-        public IChangeToken GetReloadToken()
-        {
+        public IChangeToken GetReloadToken(){
             throw new NotImplementedException();
         }
 
-        public IConfigurationSection GetSection(string key)
-        {
+        public IConfigurationSection GetSection(string key){
             throw new NotImplementedException();
         }
     }
---
+++ test/demo42tests/baseImageTests.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebUI;
+using WebUI.Pages;
+using Xunit;
+
+namespace demo42tests{
+    public class BaseImageTests{
+        [Fact]
+        public void Base_Image_Validated_Expected(){
+            var valueTested = 
+                Environment.GetEnvironmentVariable("SOMETHING_UNEXPECTED");
+
+            Console.WriteLine(
+                string.Format("valueTested: {0}, from envVar: SOMETHING_UNEXPECTED",
+                                valueTested));
+
+            Assert.True(string.IsNullOrEmpty(valueTested) ||
+                        valueTested == "Good" ||
+                        valueTested == "Amazing" ||
+                        valueTested == "Great");
+        }
+    }
+}
